3430bd614332f9bcb296f3babe90b8b4ccbd4e4d
circlindev-caleb <caleb@circlin.co.kr>
2022-01-06 02:41:20 +0900

    Fixing s3 path.

--- global_things/variables.py
+++ global_things/variables.py
@@ -3,7 +3,7 @@ SLACK_NOTIFICATION_WEBHOOK = "https://hooks.slack.com/services/T01CCAPJSR0/B02SB
 
 #S3 bucket
 BUCKET_NAME = "circlin-plus"
-BUCKET_OUTPUT_PATH = "/bodylab_picture/output"
+BUCKET_OUTPUT_PATH = "bodylab_picture/output"
 
 #Analyzed image path(local)
 ANALYZED_IMAGE_PATH = "/home/ubuntu/circlinMembersApi_python/bodylab_picture_analysis/picture_analysis/img_output"
