f03536fdfbffa8158947c53f958c40a801a1fcbb
circlindev-caleb <caleb@circlin.co.kr>
2022-03-01 02:42:57 +0900

    Trying to ignore config folder.

--- config/database.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    "user": "circlin",
-    "password": "circlinDev2019!",
-    "host": "circlin-test.cse1vltsv4xu.ap-northeast-2.rds.amazonaws.com",
-    "port": 3306,
-    "database": "circlin_plus_test",
-    "charset": "utf8"
-}
